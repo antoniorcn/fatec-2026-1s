9f5df061411005b100e6ab0c5d9063dc53b35f6b
Antonio Carvalho <antoniorcn@hotmail.com>
2026-05-23 10:53:10 -0300

    cpb-jd-prog2-noite aula 15 parte 4

--- cpb-jd-prog2-noite/aula15/jogo-plataforma.py
+++ cpb-jd-prog2-noite/aula15/jogo-plataforma.py
@@ -2,16 +2,31 @@ from typing import Any
 
 import pygame
 
+def recortar( spritesheet, g_id, colunas, largura, altura):
+    coluna = g_id % colunas
+    linha = g_id // colunas
+    x_img = coluna * largura
+    y_img = linha * altura
+    return spritesheet.subsurface( (x_img, y_img), (largura, altura) )
+
 class Heroi( pygame.sprite.Sprite ):
-    def __init__(self, x, y, piso, imagem ) -> None:
+    def __init__(self, x, y, piso, spritesheet, colunas, largura, altura ) -> None:
         super().__init__()
+        self.animacao = [27, 28, 29, 30, 31, 32, 33, 34, 35]
+        self.frame = 0
+        self.g_id = 0
         self.x = x
         self.y = y
         self.piso = piso
         self.vel_x = 0
         self.vel_y = 0
-        self.rect = pygame.Rect( (x, y), (128, 128) )
-        self.image = imagem
+        self.rect = pygame.Rect( (x, y), (largura, altura) )
+        self.spritesheet = spritesheet
+        self.colunas = colunas
+        self.largura = largura
+        self.altura = altura
+        self.image = recortar(spritesheet, self.animacao[self.g_id],
+                              colunas, largura, altura)
 
     def update(self) -> None:
         super().update()
@@ -20,6 +35,13 @@ class Heroi( pygame.sprite.Sprite ):
         self.rect.move_ip((self.vel_x, int(self.vel_y)))
         if self.rect.y >= self.piso:
             self.rect.y = self.piso
+        self.image = recortar(self.spritesheet, self.animacao[ self.g_id ],
+                              self.colunas, self.largura, self.altura)
+        if self.frame % 50 == 0:
+            self.g_id = self.g_id + 1
+        if self.g_id >= self.colunas:
+            self.g_id = 0
+        self.frame += 1
 
 
 class Boss( pygame.sprite.Sprite ):
@@ -46,20 +68,20 @@ tela = pygame.display.set_mode( (LARGURA, ALTURA), 0, 32 )
 
 cenario1 = pygame.image.load("./images/cenario1.png").convert_alpha()
 cenario2 = pygame.image.load("./images/cenario2.png").convert_alpha()
-heroi = pygame.image.load("./images/heroi.png")\
-    .convert_alpha()\
-    .subsurface( ((0, 192), (64, 64)) )
-boss = pygame.image.load("./images/boss.png")\
-    .convert_alpha()\
-    .subsurface( ((96, 96), (96, 96)) )
+heroi_spritesheet = pygame.image.load("./images/heroi.png")\
+    .convert_alpha()
+heroi_spritesheet = pygame.transform.scale(heroi_spritesheet, (1152, 512))
+    
+boss_spritesheet = pygame.image.load("./images/boss.png")\
+    .convert_alpha()
+boss = recortar( boss_spritesheet, 12, 4, 96, 96)
 
 cenario1_ready = pygame.transform.scale(cenario1, (LARGURA, ALTURA))
 cenario2_ready = pygame.transform.scale(cenario2, (LARGURA, ALTURA))
-heroi_ready = pygame.transform.scale(heroi, (128, 128))
 boss_ready = pygame.transform.scale(boss, (128, 128))
 
 
-heroi_sprite = Heroi(50, 4, 400, heroi_ready)
+heroi_sprite = Heroi(50, 4, 400, heroi_spritesheet, 9, 128, 128)
 boss_sprite = Boss(650, 400, boss_ready)
 
 personagens = pygame.sprite.Group()
